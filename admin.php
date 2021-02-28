717e3e27eeb2a929eb08bfe44fdb2ab2eb53b941
anubhav_sharma <anubhav.sharma@itgopeshwar.ac.in>
2021-02-28 17:34:04 +0530

    Add files via upload
    2nd update

--- admin.php
+++ admin.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,7 +16,7 @@ table, th, td {
 <body>
 
 <form action="" method="get">
-<label for="option">Choose a topic:</label>
+<label for="option">Choose a Department:</label>
 
 <select name="option" id="option">
   <option value="agriculture">agriculture</option>
@@ -25,7 +27,6 @@ table, th, td {
 </select>
     <input type="submit" name="submit" value="submit"/>
 </form>
-
 <?php
     $servername = "localhost";
     $username = "root";
@@ -39,34 +40,12 @@ table, th, td {
        if(isset($_GET['submit'])){
       $selected_val = $_GET['option'];
       echo "You have selected:" .$selected_val;
-      echo ("<br> ");
-     
-     
-        
+      echo ("<br> "); 
       $sql = "select * from users where options='$selected_val'";
       
       $result = mysqli_query($mysqli, $sql);
       
-      //  while($row = mysqli_fetch_row($result))
-      // {
-      //     echo ($row); 
           
-      // }
-      if (mysqli_num_rows($result) > 0) {
-        echo "<table><tr><th>aadhar</th><th>contact</th><th>name</th><th>father name</th><th>address</th><th>options</th></tr>";
-
-        // output data of each row
-        while($row = mysqli_fetch_assoc($result)) {
-        //  echo " aadhar " . $row["aadhar_no"]. " contact No " . $row["contact_no"]. " name " . $row["name"].  " father's Name " . $row["fathers_name"].  " address " . $row["address"].  " options " . $row["options"]."<br>";
-        echo "<tr><td>".$row["aadhar_no"]."</td><td>".$row["contact_no"]."</td><td> ".$row["name"]."</td><td>".$row["fathers_name"]."</td><td>".$row["address"]."</td><td>". $row["options"]."</td></tr>";
-
-        echo "/table";
-        }
-      } else {
-        echo "0 results";
-      }
-      
-            //while($row = mysqli_fetch_assoc($result))
       mysqli_close($mysqli);
      
       
@@ -74,12 +53,58 @@ table, th, td {
      
       }
     }
-    
+    ?>
 
+<table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">Aadhar No</th>
+      <th scope="col">contact No</th>
+      <th scope="col">Name</th>
+      <th scope="col">fathers name</th>
+      <th scope="col">Address</th>
+      <th scope="col">department</th>
+      <th scope="col">Status</th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php
+  if (mysqli_num_rows($result) > 0) {
+      
+      $i = 1;
+// output data of each row
 
-   
+while($row = mysqli_fetch_assoc($result)) {
+?>  
+ <tr > 
+ <th scope="row"><?php  echo $row ['aadhar_no']; ?> </th>
+      
+		   <td><?php echo $row ['contact_no']; ?></td>
+		   <td><?php echo $row ['name']; ?></td>
+		   <td><?php echo $row ['fathers_name']; ?></td>
+		   <td><?php echo $row ['address']; ?></td>  
+       <td><?php echo $row ['options']; ?></td>  
+       <td><?php echo $row ['status'];  ?> 
+              <button type="button" type="submit" value="accept"  class="btn btn-success"><i class="fas fa-edit">Accept</i></button>
+            <button type="button" type="submit" value="reject" class="btn btn-danger"><i class="far fa-trash-alt">Reject</i></button> </td>  
+
+		   </tr>
+		<?php } ?>
+    <?php
+
+} else {
+echo "0 results";
+}
   
 ?>
+   
+  </tbody>
+</table>
+  
 
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 </html>
